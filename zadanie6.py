a28973be58273915a854857db6bf2c2955bf86a5
Miksonxd2009 <miksonxd2009@gmail.com>
2024-11-20 21:27:26 +0100

    Update zadanie6.py

---
+++ zadanie6.py
@@ -0,0 +1,3 @@
+# Pętla while powtarza dopóki niespełni danego waruku, więc musimy ręcznie zmienić warunek, ponieważ pętla powtarzała zę w niekończoność. Pętli while możemy użyć gdy nie znamy liczby powtórzeń i wykonujemy skomplikowaną czynność.
+
+#Pętla for powinna mieć jasno określoną liczbę powtórzeń, po nadaniu ilości potórzeń sama się skończy, sotosujemy ją kiedy znamy liczę powtórzeń, które ma wykonać pętla.
